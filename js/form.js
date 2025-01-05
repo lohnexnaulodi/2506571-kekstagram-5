9b49e6edaf6c33628945e2f5ef9fbd995d224f19
Ирина Малыхина <irina.malykhina.2006@mail.ru>
2025-01-05 19:10:04 +0500

    финал

--- js/form.js
+++ js/form.js
@@ -48,7 +48,7 @@ const hasUnidueTags = (value) => {
 };
 
 function onDocumentKeydown(evt) {
-  if ((isEscapeKey(evt)) && !isTextFieldFocused()) {
+  if ((isEscapeKey(evt)) && !isTextFieldFocused && !document.querySelector('.error')()) {
     evt.preventDefault();
     hideModal();
     document.removeEventListener('keydown', onDocumentKeydown);
